0793be7623d7303eb1b95ac333f985f392660e09
AndreaMeroi <andreameroidublin@gmail.com>
2025-11-28 13:36:44 +0100

    add bonus homework

--- js-arrays /02-arrays-bonus/script.js
+++ js-arrays /02-arrays-bonus/script.js
@@ -10,17 +10,34 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+for (let i = teachers.length - 1; i >= 0; i--) {
+
+  const reversedTeachers = teachers[i]
+  console.log(reversedTeachers);
+
+}
+
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+const longNames = teachers.filter(String => String.length >= 5)
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
+teachers.splice(teachers.indexOf('Ed'), 1)
+console.log(teachers);
+
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = teachers.includes('Fabio');
+console.log(isFabioPresent);
+
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+const teachersString1 = teachers.join(',');
+console.log(teachersString1);
+
+const teachersString2 = teachers.toString();
+console.log(teachersString2);
