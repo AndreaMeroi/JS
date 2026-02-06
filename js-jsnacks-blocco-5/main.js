16bd3ac258a5c062ecfb696f7a98d921a5573c01
AndreaMeroi <andreameroidublin@gmail.com>
2026-02-06 12:41:15 +0100

    add array method exercise

---
+++ js-jsnacks-blocco-5/main.js
@@ -0,0 +1,189 @@
+// Stampa in console tutti i nomi
+const names = ['Edoardo', 'Simone', 'Francesco'];
+
+names.forEach((name) => {
+  console.log(name);
+})
+
+
+// Stampa in console tutti i nomi
+
+const people = [
+  { name: 'Paolo', age: 35 },
+  { name: 'Giulia', age: 24 },
+  { name: 'Marco', age: 67 }
+];
+
+people.forEach((nameOnly) => {
+  console.log(nameOnly.name);
+})
+
+
+// Crea un nuovo array in cui tutti i numeri siano incrementati di 1
+
+const numbers = [2, 8, 4, 7, 2, 87];
+
+const numbersPiùUno = numbers.map((numberPiùUno) => {
+  return `${numberPiùUno + 1}`
+})
+console.log(numbersPiùUno);
+
+
+// Crea un nuovo array coi nomi di tutti gli autori
+
+const posts = [
+  { author: 'Marco', date: '12/3/2021', content: 'lorem ipsum...' },
+  { author: 'Luca', date: '30/6/2021', content: 'lorem ipsum...' },
+  { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
+  { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
+]
+
+const authors = posts.map((authorName) => {
+  return `${authorName.author}`
+})
+console.log(authors);
+
+
+
+// Crea un array a partire dall'array nums, che abbia solo i numeri pari
+
+
+const nums = [2, 8, 4, 7, 12, 87];
+
+const numeriPari = nums.filter(numeroPari => numeroPari % 2 === 0)
+console.log(numeriPari);
+/*
+ESTESA
+const numeriPari = nums.filter((numeroPari)=>{
+if (numeroPari % 2 === 0){
+  return true
+}
+return false 
+})
+console.log(numeriPari);
+*/
+
+
+// A partire dall'array fornito, crea due array. Uno con le matite lunghe almeno 15cm. L'altro con le restanti.
+
+const pencils = [
+  { brand: 'Faber-Castell', weight: 8, sizeInCm: 12 },
+  { brand: 'Staedtler', weight: 9, sizeInCm: 18 },
+  { brand: 'Faber-Castell', weight: 7, sizeInCm: 10 },
+  { brand: 'Staedtler', weight: 10, sizeInCm: 16 },
+  { brand: 'Faber-Castell', weight: 6, sizeInCm: 14 },
+  { brand: 'Derwent', weight: 5, sizeInCm: 20 },
+  { brand: 'Derwent', weight: 4, sizeInCm: 11 },
+  { brand: 'Staedtler', weight: 6, sizeInCm: 9 },
+  { brand: 'Caran d’Ache', weight: 12, sizeInCm: 22 },
+  { brand: 'Caran d’Ache', weight: 11, sizeInCm: 13 },
+];
+
+const biggerPencils = pencils.filter(biggerPencil => biggerPencil.sizeInCm >= 15)
+console.log(biggerPencils);
+
+const shorterPencils = pencils.filter(shorterPencil => shorterPencil.sizeInCm < 15)
+console.log(biggerPencils);
+
+/*
+ESTESA
+const biggerPencils = pencils.filter((biggerPencil)=>{
+  console.log(biggerPencil.sizeInCm);
+  
+  if (biggerPencil.sizeInCm >= 15){
+    return true
+  }
+  return false
+})
+console.log(biggerPencils);
+
+const shorterPencils = pencils.filter((shorterPencil)=>{
+  console.log(shorterPencil.sizeInCm);
+  
+  if (shorterPencil.sizeInCm < 15){
+    return true
+  }
+  return false
+})
+console.log(biggerPencils);
+*/
+
+
+// Recupera i dati dello studente con id 2
+
+
+const students = [
+  {
+    id: 1,
+    name: 'Marco Lanci',
+    age: 32,
+    class: '3C'
+  },
+  {
+    id: 2,
+    name: 'Mario Banfi',
+    age: 34,
+    class: '4A'
+  },
+  {
+    id: 3,
+    name: 'Luigi Banzi',
+    age: 33,
+    class: '5B'
+  },
+];
+
+const singleStudent = students.find(student => student.id === 2)
+
+console.log(singleStudent)
+/*
+ESTESA
+const singleStudent = students.find((student)=>{
+  return student.id === 2
+})
+console.log(singleStudent);
+*/
+
+
+// Recuperare la classe dello studente Marco Lanci
+
+
+const students_2 = [
+  {
+    id: 1,
+    name: 'Marco Lanci',
+    age: 32,
+    classroom: '3C'
+  },
+  {
+    id: 2,
+    name: 'Mario Banfi',
+    age: 34,
+    classroom: '4A'
+  },
+  {
+    id: 3,
+    name: 'Luigi Banzi',
+    age: 33,
+    classroom: '5B'
+  }
+];
+
+
+const studentClass = students_2.find(theStudent => theStudent.name === 'Marco Lanci').classroom
+
+console.log(studentClass);
+
+
+/* 
+CON IF NON FUNZIONA Perché il metodo `find` restituisce **sempre l’intero oggetto** che soddisfa la condizione, non il valore della proprietà che ritorni nel callback.  
+
+const studentClass = students_2.find((theStudent) =>{ 
+  
+  if(theStudent.name === 'Marco Lanci')
+    {
+    return theStudent.classroom
+  }
+})
+console.log(studentClass);
+*/
